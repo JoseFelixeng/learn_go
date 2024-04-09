99669eb0f3c42825780edf12b5d969b346fa30b4
JoseFelixeng <jfelixnunes2k15@gmail.com>
2024-04-09 11:16:21 -0300

    atualizando os conhecimentos sobre GO

---
+++ anotações/pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+func escrever2() {
+	// função escrever 2 usada como teste
+	fmt.Println("Escrevendo na Função 2, auxiliar só funiona dentro do mesmo pacote!!!")
+}
